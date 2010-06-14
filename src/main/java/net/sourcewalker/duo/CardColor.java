be4a2be150353018840cfc24d75281f98f24f700
Robert <xperimental@solidproject.de>
2010-06-15 01:00:18 +0200

    Very simple first test.

---
+++ src/main/java/net/sourcewalker/duo/CardColor.java
@@ -0,0 +1,11 @@
+package net.sourcewalker.duo;
+
+/**
+ * This enumeration contains the different "colors" of playing cards. The
+ * traditional english notation is used.
+ * 
+ * @author xperimental
+ */
+public enum CardColor {
+    Clubs, Diamonds, Hearts, Spades
+}
---
+++ src/test/java/net/sourcewalker/duo/CardColors.java
@@ -0,0 +1,29 @@
+package net.sourcewalker.duo;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class CardColors {
+
+    @Test
+    public void Clubs() {
+        assertNotNull(CardColor.Clubs);
+    }
+
+    @Test
+    public void Diamonds() {
+        assertNotNull(CardColor.Diamonds);
+    }
+
+    @Test
+    public void Hearts() {
+        assertNotNull(CardColor.Hearts);
+    }
+
+    @Test
+    public void Spades() {
+        assertNotNull(CardColor.Spades);
+    }
+
+}
